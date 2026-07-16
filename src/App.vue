60a26e4ee30a8b13c3bc7f35ce416bfca4156ed0
AndersonCarvajal <andersoncarvajalromero30@gmail.com>
2026-07-16 09:44:21 -0400

    Deploy production site

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <EncabezadoApp
+    titulo="Registro de Proveedores"
+    subtitulo="Sistema de apoyo para la gestión comercial"
+  />
+
+  <FormularioProveedor />
+</template>
+
+<script setup>
+import EncabezadoApp from "./components/Encabezado.vue";
+import FormularioProveedor from "./components/form.vue";
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+</style>
---
+++ src/components/Encabezado.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>Registro de proveedores</h1>
+    <p>Sistema de apoyo para la gestión comercial</p>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+header {
+  background-color: #000a06;
+  color: white;
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  margin: 0;
+}
+
+p {
+  margin-top: 8px;
+}
+</style>
